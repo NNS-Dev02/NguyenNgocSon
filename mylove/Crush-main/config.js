202f450964b23c72931866cd1cf712b8ae34dc77
NNS-Dev02 <ngocsonlk1502@gmail.com>
2022-11-08 20:11:28 +0700

    upload weblove 3

---
+++ mylove/Crush-main/config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Bẹp ❤️",
+    introTitle: 'Yêu Bẹp 🙆‍♂️',
+    introDesc: `Trái đất vốn lạ thường
+    mà sao em cứ đi nhầm đường,
+    lạc vào tim anh lẻ loi.`,
+    btnIntro: 'Bấm đi :>',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy! ',
+    btnYes: 'Yêu Sơn! 😘',
+    btnNo: 'Không nha 😏',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu tớ. 😝',
+    btnReply: 'Gửi Sơn ❤️',
+    reply: 'Yêu thì yêu mà không yêu thì buộc yêu =)))',
+    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘',
+    messDesc: 'Tối nay 8h, nhắn tin chơi nha 😉',
+    btnAccept: 'Okiii lun 💕',
+    messLink: 'https://www.facebook.com/messages/t/100006465238671'
+}
