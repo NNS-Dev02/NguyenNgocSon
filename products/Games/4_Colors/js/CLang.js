556dcdd3c0e6c157dcb176e78325a253cdc23b8f
NNS-Dev02 <ngocsonlk1502@gmail.com>
2022-11-28 01:08:54 +0700

    add more game

---
+++ products/Games/4_Colors/js/CLang.js
@@ -0,0 +1,32 @@
+TEXT_GAMEOVER  = "Xin chức mừng! Bạn đã chiến thắng!";
+TEXT_LOSE = "Rất tiếc, Người chơi ";
+TEXT_LOSE2 = " đã thắng trò chơi này.";
+TEXT_SCORE     = "Điểm của bạn";
+TEXT_RESTART   = "CHƠI LẠI";
+TEXT_MOVES      = "MOVES";
+TEXT_ARE_SURE = "BẠN CÓ CHẮC KHÔNG?";
+TEXT_SELECT_PLAYERS = "CHỌN SỐ NGƯỜI CHƠI!";
+TEXT_SELECT_COLOR = "CHỌN MÀU ";
+TEXT_TUTORIAL1 = "Chào mừng đến với 4 COLORS!\n\n Đấu thẻ bài theo màu sắc hoặc số và hãy là người đầu tiên hết thẻ bài trên tay!";
+TEXT_TUTORIAL2 = "Thẻ bài chức năng!!\n\nThẻ Bài +2\n\nNgười chơi tiếp theo sẽ bị mất lượt chơi và rút thêm 2 thẻ bài.";
+TEXT_TUTORIAL3 = "Cấm lượt đi của người chơi tiếp theo.";
+TEXT_TUTORIAL4 = "Thẻ bài chức năng!!\n\nThẻ Bài Đảo Chiều\n\nĐảo lộn trật tự lượt đi và đổi sang chiều ngược lại.";
+TEXT_TUTORIAL5 = "Thẻ bài chức năng!!\n\nThẻ Bài Wild Cards\n\nNgười sở hữu có quyền chọn màu sắc cho vòng chơi tiếp theo.";
+TEXT_TUTORIAL6 = "Thẻ bài chức năng!!\n\nThẻ Bài +4 Wild Cards\n\nNgười chơi tiếp theo sẽ bị mất lượt chơi và rút thêm 4 thẻ bài và người sở hữu có quyền chọn màu sắc cho vòng chơi tiếp theo..";
+TEXT_TUTORIAL7 = "Đừng quên nhấn vào hình này khi bạn chỉ còn 1 thẻ bài trên tay( hoặc sẽ bị phạt rút thêm 2 thẻ bài! )";
+TEXT_PLAYER_1 = "NGƯỜI CHƠI 1";
+TEXT_PLAYER_2 = "NGƯỜI CHƠI 2";
+TEXT_PLAYER_3 = "NGƯỜI CHƠI 3";
+TEXT_PLAYER_4 = "NGƯỜI CHƠI 4";
+TEXT_ALERT_1 = "Bạn phải nhấn vào hình này\n<-----\nkhi bạn chỉ còn 1 thẻ bài trên tay. Bạn bị phạt rút thêm 2 thẻ bài!";
+var TEXT_PRELOADER_CONTINUE = "BẮT ĐẦU";
+var TEXT_DEVELOPED = "4 COLORS PHÁT TRIỂN BỞI     ";
+
+
+TEXT_SHARE_IMAGE = "200x200.jpg";
+TEXT_SHARE_TITLE = "Congratulations!";
+TEXT_SHARE_MSG1 = "You collected <strong>";
+TEXT_SHARE_MSG2 = " points</strong>!<br><br>Share your score with your friends!";
+TEXT_SHARE_SHARE1 = "My score is ";
+TEXT_SHARE_SHARE2 = " points! Can you do better";
+
